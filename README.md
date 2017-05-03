73107e659d6702b6f26aa00bd1c65efb16771799
Ben Elsworth <be15516@bristol.ac.uk>
2017-05-03 16:32:16 +0100

    added Ed's script

--- README.md
+++ README.md
@@ -3,3 +3,68 @@
 Phenotype submission - https://github.com/MRCIEU/BiobankPhenotypes/wiki
 
 Pipeline code -  https://github.com/MRCIEU/BiobankGWAS
+
+Phenotype creation:
+
+UK Biobank Pheno Extractor
+===================
+
+Extract required variables from UK Biobank R files without loading everything to memory. Still requires that there is enough RAM to fit extracted variables in.
+
+## Requires
+- Python 2.7
+- Pandas (Python module)
+
+## Run
+
+```
+usage: extractUKBBpheno_v0.2.py [-h] --indata <file> [<file> ...] --req <file>
+                                --out <file> [--sep <str>]
+
+Extract data from UKBB.
+
+arguments:
+  -h, --help            show this help message and exit
+  --indata <file> [<file> ...]
+                        Input data files
+  --req <file>          File containing list of required variables
+  --out <file>          Output file name including extension
+  --link <file>         Link file for project ID -> genetic ID conversion
+  --sep <str>           Column separator (default: tab)
+```
+
+### In data files
+Input data must be tab file from the `.../data/derived/format/r/` directory. Multiple tab files can be provided and variables will be merged using column 1 (eid) as an index.
+
+### Link file
+Comma separated file with header on first line. Column 1 is your project specific ID, column 2 is the genetic IDs.
+
+### Required variables list
+Is a file where the first column is a list of variable IDs that are needed. IDs are the same as those in the Data Showcase.
+
+### Example
+`required_variables.txt`:
+
+```
+21
+34
+6141
+2385
+2395
+3062
+```
+
+Script:
+
+```
+#!/usr/bin/env bash
+#
+
+datadir=/path/to/data/derived/format/r
+
+python extractUKBBpheno_v0.1.py \
+  --indata $datadir/data.4263.tab $datadir/data.4894.tab $datadir/data.5013.tab \
+  --link linker_file.csv
+  --req required_variables.txt \
+  --out output_extracted_variables.tsv
+```
---
+++ scripts/extractUKBBpheno_v0.2.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Ed Mountjoy
+#
+# Efficiently extract phenotype data from UKBB files
+#
+
+import sys
+import argparse
+import pandas as pd
+
+def main():
+
+    # Args
+    args = parse_arguments()
+
+    print 'Load list of required variables'
+    reqvars = load_file_to_list(args.req, 0, args.sep)
+
+    print 'Check required variales are in data files'
+    check_variables_available(reqvars, args.indata, args.sep)
+
+    print 'Extract required variables from each dataset separately'
+    reqdata = []
+    for infile in args.indata:
+        # Find names of columns to extract
+        reqcols = find_col_names(reqvars, infile, args.sep)
+        # Load data using pandas
+        df = pd.read_csv(infile, sep=args.sep, index_col=0, usecols=reqcols,
+                         header=0, dtype=str)
+        reqdata.append(df)
+
+    print 'Concatenate into a single data frame'
+    outdf = pd.concat(reqdata, axis=1)
+    # Order columns based on input list
+    colorder = sorted(list(outdf.columns),
+                      key=lambda col: reqvars.index(col.split(".")[1]))
+    outdf = outdf.loc[:, colorder]
+
+    print 'Link projectID to geneticID'
+    outdf.insert(0, "projectID", outdf.index)
+    if args.link:
+        # Load linker IDs to dictionary
+        mapdf = pd.read_csv(args.link, sep=",", header=0)
+        mapdict = dict(zip(mapdf.iloc[:, 0], mapdf.iloc[:, 1]))
+        # Make genetic ID list
+        genID = [mapdict.get(projID, "NA") for projID in outdf["projectID"]]
+        # Add genetic IDs
+        outdf.insert(1, "geneticID", genID)
+
+    # Write output
+    outdf.to_csv(args.out, sep=args.sep, na_rep="NA", index=False)
+
+    return 0
+
+def find_col_names(reqvars, indata, sep="\t"):
+    """ Returns list of column names that need extracting.
+    """
+    # Covert req to set
+    reqset = set(reqvars)
+    # Load header
+    header = load_header(indata, sep)
+    # Get list of required colnames
+    reqcols = [header[0]]
+    for col in header[1:]:
+        if col.split(".")[1] in reqset:
+            reqcols.append(col)
+    return reqcols
+
+def check_variables_available(reqvars, indata, sep="\t", quit=True):
+    """ Checks that all required variables are available in data.
+        If quit == True, then quit if any variables are missing.
+    """
+    # Load available variables from data sets
+    availvars = set([])
+    for infile in indata:
+        header = load_header(infile, sep)
+        for col in header[1:]:
+            var = col.split(".")[1]
+            availvars.add(var)
+    # Check required vars are availabe
+    notpresent = []
+    for reqvar in reqvars:
+        if not reqvar in availvars:
+            notpresent.append(reqvar)
+    # Quit if any variables not present
+    if len(notpresent) > 0:
+        print "Error: Variables not present in datasets:"
+        for var in notpresent:
+            print " {0}".format(var)
+        if quit:
+            sys.exit("Exiting!")
+
+    return 0
+
+def load_header(infile, sep="\t"):
+    """ Loads header from file
+    """
+    with open(infile, "r") as in_h:
+        header = in_h.readline().rstrip().split(sep)
+        return header
+
+def load_file_to_list(infile, col=0, sep="\t"):
+    """ Loads a file to a list
+    """
+    l = []
+    with open(infile, "r") as in_h:
+        for line in in_h:
+            l.append(line.rstrip().split(sep)[col])
+    return l
+
+def parse_arguments():
+    """ Parses command line arguments.
+    """
+    # Create top level parser.
+    parser = argparse.ArgumentParser(description="Extract data from UKBB.")
+
+    # Add options
+    parser.add_argument('--indata', metavar="<file>",
+        help=('Input data files'),
+        required=True,
+        nargs="+",
+        type=str)
+    parser.add_argument('--req', metavar="<file>",
+        help=('File containing list of required variables'),
+        required=True,
+        type=str)
+    parser.add_argument('--out', metavar="<file>",
+        help=('Output file name including extension'),
+        required=True,
+        type=str)
+    parser.add_argument('--link', metavar="<file>",
+        help=('Link file for project ID -> genetic ID conversion'),
+        required=False,
+        type=str)
+    parser.add_argument('--sep', metavar="<str>",
+        help=('Column separator (default: tab)'),
+        default='\t',
+        type=str)
+
+    # Parse the arguments
+    args = parser.parse_args()
+
+    # Parse the arguments
+    return args
+
+if __name__ == '__main__':
+
+    main()
